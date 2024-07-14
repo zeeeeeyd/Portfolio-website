3c319485c5f4805cd8fde98c92fb645784402fc6
Zeyd SAIAD <zeyd.saiad@etu.univ-mosta.dz>
2024-07-14 22:26:42 +0100

    Adding Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
@@ -6,25 +6,36 @@ import '../styles/Header.css';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const { t } = useTranslation();
 
   const handleClick = () => {
     navigate('/');
   }
-  
+
+  const toggleMenu = () => {
+    console.log('Toggle menu called');
+    setIsOpen(!isOpen);
+    console.log('isOpen:', !isOpen);
+  }
+
   return (
     <header className="header">
       <button onClick={handleClick} className="logo">Sirius</button>
-      <nav className="nav">
-        <Link style={{cursor:'pointer'}} to="about" smooth={true} duration={500}>{t('About Us')}</Link>
-        <Link style={{cursor:'pointer'}} to="faq" smooth={true} duration={500}>{t('FAQ')}</Link>
-        <Link style={{cursor:'pointer'}} to="blog" smooth={true} duration={500}>{t('Blog')}</Link>
-        <Link style={{cursor:'pointer'}} to="features" smooth={true} duration={500}>{t('Features')}</Link>
+      <nav className={`nav ${isOpen ? 'open' : ''}`} style={{ display: isOpen ? 'flex' : '' }}>
+        <Link style={{ cursor: 'pointer' }} to="about" smooth={true} duration={500}>{t('About Us')}</Link>
+        <Link style={{ cursor: 'pointer' }} to="faq" smooth={true} duration={500}>{t('FAQ')}</Link>
+        <Link style={{ cursor: 'pointer' }} to="blog" smooth={true} duration={500}>{t('Blog')}</Link>
+        <Link style={{ cursor: 'pointer' }} to="features" smooth={true} duration={500}>{t('Features')}</Link>
         <LanguageSwitcher />
         <button className="chat">{t("Let's Talk")}</button>
       </nav>
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className="hamburger-line"></span>
+        <span className="hamburger-line"></span>
+        <span className="hamburger-line"></span>
+      </button>
     </header>
   );
 };
